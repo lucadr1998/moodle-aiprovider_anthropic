45fc5aebb52adf7471c0249be8849505bf015b5d
LucaDR1998 <lucademichelirubio.portfolio@gmail.com>
2026-03-22 12:06:08 +0100

    Version increased

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_anthropic';
-$plugin->version = 2026032200;
+$plugin->version = 2026032201;
 $plugin->requires = 2025100600;
 $plugin->release = '1.0.1';
 $plugin->maturity = MATURITY_STABLE;
